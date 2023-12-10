d29a8e7c493674ce37e9433e6c358a9ad58b0709
Krunal-Konnects <153419563+Krunal-Konnects@users.noreply.github.com>
2023-12-10 09:13:52 +0000

    datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+
+/* 
+number
+string
+boolean 
+bigInt
+boolean 
+null
+undefined
+symbol , it is used to define components , applicable to figma files 
+*/
+
+// to check the datatype you can always use typeof() function for eg.
+
+console.log(typeof "krunal");
+console.log(typeof undefined);
+console.log(typeof null);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_ID = 144553
+let account_email = "abc123@gmail.com"
+var account_Password = "12345"
+account_City="Jaipur"
+let account_state;
+
+//account_ID=5 not allowed
+
+console.log(account_ID);
+
+console.table([account_email,account_Password,account_City,account_state]);
+console.log(typeof "krunal");
+
+// null is a object 
+
+
